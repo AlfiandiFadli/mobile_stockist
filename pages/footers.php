830b0e6823affa88b389ac9a35483591e5fafae4
alfiandifadli <34903991+AlfiandiFadli@users.noreply.github.com>
2018-03-10 01:37:29 +0000

    Create footers.php

---
+++ pages/footers.php
@@ -0,0 +1,26 @@
+<div class="py-5 bg-dark text-white">
+<div class="container">
+    <div class="row">
+    <div class="col-md-6 text-center align-self-center">
+        <p class="mb-5"> <strong>Owner</strong>
+        <br>Fuad S. Sos
+        <br>Alfiandi Fadli
+        <br>Perhatian! : Halaman ini tidak boleh di operasikan selain persetujuan pemilik toko</p>
+        <div class="my-3 row">
+        <div class="col-4">
+            <a href="https://www.facebook.com" target="_blank"><i class="fa fa-3x fa-facebook"></i></a>
+        </div>
+        <div class="col-4">
+            <a href="https://twitter.com" target="_blank"><i class="fa fa-3x fa-twitter"></i></a>
+        </div>
+        <div class="col-4">
+            <a href="https://www.instagram.com" target="_blank"><i class="fa fa-3x fa-instagram"></i></a>
+        </div>
+        </div>
+    </div>
+    <div class="col-md-6 p-0">
+        <img class="img-fluid" src="https://maps.googleapis.com/maps/api/staticmap?key=AIzaSyDW8nO9JhT_pEjebobq9pgUF2cEp0EUb1I&amp;&amp;markers=folsom+Ave+san+francisco&amp;center=folsom+Ave+san+francisco&amp;zoom=16&amp;size=640x300&amp;sensor=false&amp;scale=2"> </div>
+    </div>
+</div>
+</div>
+  
