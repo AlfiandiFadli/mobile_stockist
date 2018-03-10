9c73bf276788fa0a30fc621f07cf5f91ffcaceed
alfiandifadli <34903991+AlfiandiFadli@users.noreply.github.com>
2018-03-10 01:40:57 +0000

    Create navbar.php

---
+++ pages/navbar.php
@@ -0,0 +1,13 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-light border border-primary">
+    <div class="container">
+      <a class="navbar-brand" href="#"></a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbar3SupportedContent" aria-controls="navbar3SupportedContent" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon bg-primary"></span> </button>
+      <div class="collapse navbar-collapse text-center justify-content-center" id="navbar3SupportedContent">
+        <a class="ml-3 btn navbar-btn btn-primary" href="../barang_ms/barang_ms.php">Barang MS</a>
+        <a class="ml-3 btn navbar-btn btn-primary" href="../dipinjam/barang_dipinjam.php">Dipinjam</a>
+        <a class="ml-3 btn navbar-btn btn-primary" href="../transaksi/transaksi.php">Transaksi</a>
+        <a class="ml-3 btn navbar-btn btn-success" href="../admin/index.php">Home</a>
+        <a class="btn navbar-btn ml-2 text-white btn-secondary" href="../../includes/keluar_masuk/keluar.php"><i class="fa d-inline fa-lg fa-user-circle-o"></i> Keluar</a>
+      </div>
+    </div>
+</nav>
